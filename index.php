cf23c0d627ee19b66ec1be0c492d64e8e2a31be2
AlessioConsolo <alessioconsolo3@gmail.com>
2024-09-04 18:54:27 +0200

    [UPD] added form

--- index.php
+++ index.php
@@ -3,9 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP Badwords</title>
 </head>
 <body>
-    
+    <form action="secondpage.php" method="post">
+        <input type="text" name="name" id="" placeholder="Nome">
+        <input type="text" name="cognome" id="" placeholder="Cognome">
+        <input type="submit" value="Invia">
+    </form>
 </body>
 </html>
